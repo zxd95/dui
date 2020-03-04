39886b9ed477eba3959727bf374a7d957abf9788
zxd95 <774793337@qq.com>
2020-03-04 22:10:22 +0800

    dui project init

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-04 22:06:08
+ * @LastEditTime: 2020-03-04 22:06:11
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /dui/vue.config.js
+ */
