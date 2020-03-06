1e950e62949ed36681f30abee5f0cb09a44b352c
zhangxiaodong <774793337@qq.com>
2020-03-06 11:33:17 +0800

    Update App.vue

--- examples/App.vue
+++ examples/App.vue
@@ -1,8 +1,41 @@
 <template>
   <div id="app">
-    <dui-button>按钮</dui-button>
+    <div class="row">
+      <dui-button>默认按钮</dui-button>
+      <dui-button type="primary">主要按钮</dui-button>
+      <dui-button type="success">成功按钮</dui-button>
+      <dui-button type="warning">警告按钮</dui-button>
+      <dui-button type="danger">危险按钮</dui-button>
+    </div>
+    <div class="row">
+      <dui-button plain>默认按钮</dui-button>
+      <dui-button plain type="primary">主要按钮</dui-button>
+      <dui-button plain type="success">成功按钮</dui-button>
+      <dui-button plain type="warning">警告按钮</dui-button>
+      <dui-button plain type="danger">危险按钮</dui-button>
+    </div>
+    <div class="row">
+      <dui-button round>默认按钮</dui-button>
+      <dui-button round type="primary">主要按钮</dui-button>
+      <dui-button round type="success">成功按钮</dui-button>
+      <dui-button round type="warning">警告按钮</dui-button>
+      <dui-button round type="danger">危险按钮</dui-button>
+    </div>
+    <div class="row">
+      <dui-button circle icon="icon-search"></dui-button>
+      <dui-button circle icon="icon-edit" type="primary"></dui-button>
+      <dui-button circle icon="icon-check" type="success"></dui-button>
+      <dui-button circle icon="icon-message" type="warning"></dui-button>
+      <dui-button circle icon="icon-delete" type="danger"></dui-button>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+.row {
+  margin-top: 10px;
+  .dui-button {
+    margin-left: 10px;
+  }
+}
 </style>
