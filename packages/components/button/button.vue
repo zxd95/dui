aa28868ac095e4ff8236be7a1e1ce1e1ab738458
zhangxiaodong <774793337@qq.com>
2020-03-06 11:32:50 +0800

    Update button.vue

--- packages/components/button/button.vue
+++ packages/components/button/button.vue
@@ -1,8 +1,5 @@
 <template>
-  <button
-    class="dui-button"
-    :class="classes"
-  >
+  <button :class="classes">
     <span>
       <slot></slot>
     </span>
@@ -10,19 +7,53 @@
 </template>
 
 <script>
-// const cls = 'dui-btn'
+const preCls = 'dui-button'
 
 export default {
+  // 方便组件注册找到组件名
   name: 'DuiButton',
+  // props类型校验
+  props: {
+    type: {
+      type: String,
+      default: 'default'
+    },
+    plain: {
+      type: Boolean,
+      default: false
+    },
+    round: {
+      type: Boolean,
+      default: false
+    },
+    circle: {
+      type: Boolean,
+      default: false
+    },
+    icon: {
+      type: String,
+      default: ''
+    }
+  },
   computed: {
     classes () {
-      return {
-      }
+      return [
+        `${preCls}`,
+        `${preCls}--${this.type}`,
+        {
+          'is-plain': this.plain,
+          'is-round': this.round,
+          'is-circle': this.circle
+        }
+      ]
     }
+  },
+  created () {
+    // console.log(this.plain)
   }
 }
 </script>
 
 <style lang="scss">
-@import "./../../../src/styles/components/button.scss";
+@import './../../../src/styles/components/button.scss';
 </style>
